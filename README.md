20beea206f0d2938faad5fa368494911b019c551
Miguel Cardoso <a39233@alunos.ipb.pt>
2020-08-10 15:33:57 +0100

    learning about sequelize

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ learning NODEjs
 
 projeto do video no youtube 
 https://www.youtube.com/watch?v=OFrnB7Bvznk&list=PLJ_KhUnlXUPtbtLwaxxUxHqvcNQndmI4B&index=24
+
+Use sequelize to connect database mysql
--- index.js
+++ index.js
@@ -36,6 +36,16 @@ const Post = require('./models/Post')
         })
     })
 
+    app.get('/deletar/:id', function(req, res){
+        Post.destroy({where: {'id': req.params.id}}).then(function() {
+            res.send("Postagem deletada com sucesso")
+        }).catch(function(erro){
+            res.send("Esta postagem nao existe")
+        })
+    })
+
+
+
 app.listen(8081, function(){
     console.log("Servidor rodando")
 })
